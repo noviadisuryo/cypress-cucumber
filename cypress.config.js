02c97b918b8a971950370812d4cdc2fa41a3c5f0
Noviadi Suryo <noviadi@wartek.belajar.id>
2022-07-15 21:46:59 +0700

    add cypress cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,36 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+
+//If using this approach, just call the key "setupNodeEvents" in the E2E configurations
+// async function setupNodeEvents(on, config) {
+//   await addCucumberPreprocessorPlugin(on, config);
+//   on(
+//     "file:preprocessor",
+//     createBundler({
+//       plugins: [createEsbuildPlugin(config)],
+//     })
+//   );
+//   return config;
+// }
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+      on("file:preprocessor", bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
     },
+    specPattern: "cypress/e2e/features/*.feature",
+    baseUrl: "https://www.saucedemo.com",
+    chromeWebSecurity: false,
   },
 });
